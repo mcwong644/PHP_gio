2f6e23c842fab84ec355d89fe5a9d7dfa90b05fb
mcwong644 <mcwong644@gmail.com>
2023-10-25 23:39:01 +0800

    oct25

---
+++ dk16_index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo '<h1> Dani Krossing Lesson 16 Create a Database in MySQL</h1>';
+    // https://youtu.be/H2XtR1zwg6s?si=T95Hy7BYCD0OqpFt
+    
+    
+?>
+
+    
+</body>
+</html>
